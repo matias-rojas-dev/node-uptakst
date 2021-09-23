37f79fc17c53332409892cf67b4e6aa54bc93601
Matias Rojas <maigrojas.dev@gmail.com>
2021-09-23 17:53:01 -0300

    Re-push previews commit to up the gitignore folders

--- config/email.js
+++ config/email.js
@@ -1,6 +1,6 @@
 module.exports = {
-    user: 'daaaf33506213d',
-    password: '75a2fea009c9c3',
-    host: 'smtp.mailtrap.io',
-    port: '2525',
+    user: process.env.MAILTRAP_CREDDENTIALS_USER,
+    password: process.env.MAILTRAP_CREDDENTIALS_PASSWORD,
+    host: process.env.MAILTRAP_CREDDENTIALS_HOST,
+    port: process.env.MAILTRAP_CREDDENTIALS_PORT,
 }
